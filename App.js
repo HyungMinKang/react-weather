3b5298c3494d673ed1c9dae6db1f6cc202d36b55
HyungMinKang <ramkid91@gmail.com>
2020-05-21 09:39:55 +0900

    expo setting, react-native style(flex)

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Loading from "./Loading";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Loading />
+      <View style={styles.yellowView}></View>
+      <View style={styles.blueView}></View>
     </View>
   );
 }
 
+// react-native flexbox는 flex-direction default가 column이다
 const styles = StyleSheet.create({
+  // 형제가 없는경우 flex:1은 전체를 다 쓰겠다
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    color: "black",
+  },
+  // 형제가 있는 경우 상대적인 비율
+  yellowView: {
+    flex: 1,
+    backgroundColor: "yellow",
+  },
+  blueView: {
+    flex: 1,
+    backgroundColor: "blue",
   },
 });
---
+++ Loading.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text>Getting the fucking weather</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
